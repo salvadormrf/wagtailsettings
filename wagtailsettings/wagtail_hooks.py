0a2ebf06db96666d35965d9b4943d88b326b1914
Salvador Faria <salvador.mrf@gmail.com>
2014-11-09 19:33:59 +0000

    Updates hooks include to use wagtailcore instead of wagtailadmin

--- wagtailsettings/wagtail_hooks.py
+++ wagtailsettings/wagtail_hooks.py
@@ -2,20 +2,21 @@ from django.conf.urls import include, url
 from django.core import urlresolvers
 from django.utils.translation import ugettext_lazy as _
 
-from wagtail.wagtailadmin import hooks
+from wagtail.wagtailcore import hooks
 from wagtail.wagtailadmin.menu import MenuItem
 
 from wagtailsettings import urls
 from wagtailsettings.permissions import user_can_edit_settings
 
 
+@hooks.register('register_admin_urls')
 def register_admin_urls():
     return [
         url(r'^settings/', include(urls)),
     ]
-hooks.register('register_admin_urls', register_admin_urls)
 
 
+@hooks.register('construct_main_menu')
 def construct_main_menu(request, menu_items):
     if user_can_edit_settings(request.user):
         menu_items.append(MenuItem(
@@ -24,4 +25,3 @@ def construct_main_menu(request, menu_items):
             classnames='icon icon-cogs',
             order=1000
         ))
-hooks.register('construct_main_menu', construct_main_menu)
